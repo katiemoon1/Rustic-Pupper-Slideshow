6160766416b07b71a0ccc79b5b7ae7529ecc5a01
Moon Child <kaitline.moon@gmail.com>
2018-09-23 10:46:25 -0400

    Added pictures and started working on the JS.

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+
+// Array with pictures of Lux
+var luxPhotos = ["assets/images/lux-1.jpg", "assets/images/lux-2.jpg", "assets/images/lux-3.jpg", "assets/images/lux-4.jpg", "assets/images/lux-5.jpg", "assets/images/lux-6.jpg"];
+
+
+
+
+
+
+})
