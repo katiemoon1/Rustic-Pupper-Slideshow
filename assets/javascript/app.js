bf0fc6ff6d7e9b11bf853eeb6f921597a2df17f6
Moon Child <kaitline.moon@gmail.com>
2018-09-23 11:39:24 -0400

    Linked jQuery and fixed skeleton stylesheet linking, started forming JS.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,9 +3,35 @@ $(document).ready(() => {
 // Array with pictures of Lux
 var luxPhotos = ["assets/images/lux-1.jpg", "assets/images/lux-2.jpg", "assets/images/lux-3.jpg", "assets/images/lux-4.jpg", "assets/images/lux-5.jpg", "assets/images/lux-6.jpg"];
 
+// Setting up the counter variable
+var count = 0;
 
+// Variable to hold the interval
+var showPicture;
 
+// Creating a function to show the pictures
+var displayPictures = function () {
+    $(".pupper").html(luxPhotos[count]);
+}
 
+// Creating a function to go to the next picture after a set interval
+var nextPicture = function () {
+    count++;
+    setTimeout(displayPictures, 2000);
+
+    if (count === luxPhotos.length) {
+        count = 0;
+    }
+}
+
+// Creating a function to start the slideshow
+var startShow = function () {
+    showPicture = setInterval(nextPicture, 2000);
+}
+
+// Calling functions to run the slideshow
+displayPictures();
+startShow();
 
 
 })
